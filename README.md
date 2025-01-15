cb6491330b79915570822bbbdd90d427da9ffc9c
KhalidOLA4783 <khaledsaroukou5455@gmail.com>
2025-01-15 10:23:30 +0100

    codess
    Version 2 : Avec la première version on devrait écrire dans la fonction main or on a fait un menu avec switch alors on devrait avoir  des fonction void.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # PROJET_3
+
+# 1 - Ajouter contact .
+
+Fonction 1 (relation_avec_point): Relation entre '.' et '@' .
+   . Recherche de la position "pos2" de '.' et "pos1" de '@'.
+   . Comparaison de "pos2" et de "pos1" 
+    - Si pos2 < pos1 alors le programme retourne '-1' mais autrement retourne 1 .
+
+Fonction 2 (verification_des_caracters): Verification de chaque lettre d'email.
--- main.c
+++ main.c
@@ -1,49 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-int n ;// n est un compteur mais global.
-struct Contact {
+struct Contacts {
 char name[200] ;
 char email_address[50];
 char phone_number[16];
-};// Declaration de  la structure de contact .
+};// Declaration de  la structure de contact.
 
-struct Contact contacts[20];  // contacts[20] une variable  struct contact global .
+struct Contacts contact;  // contacts une variable  struct contact global.
 
-int ajouter (char name[200] , char email_address[50]  ,char phone_number[16]){
-struct Contact p ;// p est juste une variable struct contact locale
-if(n>=20)  // si le compteur n est superieur ou egale a 20 (le 20 au niveau de la valeur contacts)
-    return -1 ;//alors le programme retourne -1 .
-strcpy(p.name,name);
-strcpy(p.phone_number,phone_number);
-strcpy(p.email_address,email_address) ;
-
-/* Les trois strcpy permet de copier le nom , le  numero de telephone , l'adresse dans les variables de p .*/
-contacts[n++] = p ;      //j'ai affecter la valeur de contacts a p .
+/*-------------AJOUTER CONTACT-----------------------------------------*/
+char relation_avec_point(char email_address[50])
+{
+int pos1 , pos2 ;
+for(int i = 0 ; i<strlen(email_address) ; i++)
+{
+    if(email_address[i]!='@')
+    {
+        pos1 = pos1 + 1 ;
+    }
+    else
+    {
+        break ;
+    }
 }
 
-void afficher(){
-int i ;
-
-FILE *fichier = fopen("inexistant.txt", "w");
-if (fichier == NULL) {
-printf("Erreur d'ouverture du fichier\n");
+for(int i = 0 ; i<strlen(email_address) ; i++)
+{
+    if(email_address[i]!='.')
+    {
+        pos2 = pos2 + 1 ;
+    }
+    else
+    {
+        break ;
+    }
 }
 
-else {
-    for( i = 0 ; i<n ; i++){
-    fprintf(fichier," CONTACT[%d] : \n Name : %s ; \n Email address: %s; \n Phone Number : %s \n.  ",i+1 ,contacts[i].name,contacts[i].email_address, contacts[i].phone_number);
+if(pos2 < pos1)
+{
+    return -1 ;
 }
-fclose(fichier);
+else
+{
+    return 1 ;
 }
 
-
 }
-int main(){
-int n ;
+
+ char verification_des_caracters(char email_address[50])
+{
+int n;
+for(int i = 0 ; i<strlen(email_address) ; i++)
+{
+    if( email_address[i] =='.' || email_address[i] == '_' || email_address[i]== '-' || email_address[i] == '@'  || isalnum(email_address[i]) )
+    {
+        n++ ;
+    }
+}
+if(n==strlen(email_address))
+{
+    return 1 ;
+}
+else
+    return -1 ;
+}
+
+ void Ajouter_Contacts()
+     {
+         FILE *F ;
+         F = fopen("Contacts.txt","a");//Mode de lecture.
+         fflush(stdin);
+       if ( F  == NULL){
+         printf("Erreur d'ouverture du fichier.\n");
+        }
+        else
+        {
+            printf("\n");
+            printf("----------------------Ajout de contact----------------------\n");
+            printf("\nEnter youre name---->>>>>>>>\n Name: ");
+            gets(contact.name);
+            fflush(stdin);
+            printf("\n") ;
+            printf("Enter youre email ---->>>>>>>>>\n Email : ");
+            gets(contact.email_address);
+            while(verification_des_caracters(contact.email_address) == -1 || strstr(contact.email_address, "@") == NULL || relation_avec_point(contact.email_address)== -1 || strstr(contact.email_address , " ") !=NULL  || strstr(contact.email_address,".com")==NULL)
+            {
+            printf("\n' @ ' or '.com ' or 'the number digits is lower at 16' is strictly for bidden in your email. That is why \n ") ;
+            printf("\nEnter again your e-mail -->>>>>\n Email : ");
+            gets(contact.email_address);
+            }
+            fflush(stdin);
+            do{
+            printf("\nEnter your Number ------>>>>\n Number : ");
+            gets(contact.phone_number);
+              }  while( strlen(contact.phone_number) >10 || strstr(contact.phone_number , " ") !=NULL);
+            fflush(stdin);
+            fprintf(F,"%s ; %s ; %s \n",contact.name,contact.email_address,contact.phone_number);
+            fclose(F);
+        }
+    }
+
+int main()
+{ 
 printf("Hello world");
+Ajouter_Contacts();
 return 0 ;
 }
-
 
